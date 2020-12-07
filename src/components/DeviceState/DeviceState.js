eecd96d68dd627cd8f1989bfc5eeb3af53362757
Bora Ciner <bora.ciner@audivo.com>
2020-12-07 13:25:54 +0100

    worked with hardware

--- src/components/DeviceState/DeviceState.js
+++ src/components/DeviceState/DeviceState.js
@@ -29,6 +29,13 @@ class DeviceState extends Component{
         const data = new Uint8Array([100]); // d
         this.portWriter.write(data).then(res=>{
         });
+
+        this.initializePEVStrings();
+        this.setState({  
+            printOutCom : this.printOutCom,
+            toggleToRefresh : true,
+            role : this.role  
+        })
     }
     preparePEVRelatedButtons(){
         return(
@@ -76,26 +83,36 @@ class DeviceState extends Component{
 
     initializePEVStrings(){
         this.pevStrings = [
-            {details : "PLC - PIB image reading completed OK.",found : false},
-            {details : "CM_SLAC_PARAM.REQ sent",found : false},
-            {details : "CM_SLAC_PARAM.CNF received",found : false},
-            {details : "CM_START_ATTEN_CHAR.IND sent",found : false},
-            {details : "CM_START_ATTEN_CHAR.IND sent",found : false},
-            {details : "CM_START_ATTEN_CHAR.IND sent",found : false},
-            {details : "CM_MNBC_SOUND.IND sent",found : false},
-            {details : "CM_MNBC_SOUND.IND sent",found : false},
-            {details : "CM_MNBC_SOUND.IND sent",found : false},
-            {details : "CM_MNBC_SOUND.IND sent",found : false},
-            {details : "CM_MNBC_SOUND.IND sent",found : false},
-            {details : "CM_MNBC_SOUND.IND sent",found : false},
-            {details : "CM_MNBC_SOUND.IND sent",found : false},
-            {details : "CM_MNBC_SOUND.IND sent",found : false},
-            {details : "CM_MNBC_SOUND.IND sent",found : false},
-            {details : "CM_MNBC_SOUND.IND sent",found : false},
-            {details : "CM_ATTEN_CHAR.IND received",found : false},
-            {details : "CM_ATTEN_CHAR.RSP sent",found : false},
-            {details : "CM_VALIDATE.REQ sent",found : false},
-            {details : "CM_SLAC_MATCH.CNF received. -Charger Success-",found : false},	
+            {details : "PIB image reading completed OK",display : "PIB image reading completed OK",found : false,},
+            {details : "CM_SLAC_PARAM.REQ sent",display : "CM_SLAC_PARAM.REQ sent",found : false},
+            {details : "CM_SLAC_PARAM.CNF received",display : "CM_SLAC_PARAM.CNF received",found : false},
+            {details : "CM_START_ATTEN_CHAR.IND sent",display : "CM_START_ATTEN_CHAR.IND sent",found : false},
+            {details : "CM_START_ATTEN_CHAR.IND sent",display : "CM_START_ATTEN_CHAR.IND sent",found : false},
+            {details : "CM_START_ATTEN_CHAR.IND sent",display : "CM_START_ATTEN_CHAR.IND sent",found : false},
+            {details : "CM_MNBC_SOUND.IND sent",display : "CM_MNBC_SOUND.IND sent",found : false},
+            {details : "CM_MNBC_SOUND.IND sent",display : "CM_MNBC_SOUND.IND sent",found : false},
+            {details : "CM_MNBC_SOUND.IND sent",display : "CM_MNBC_SOUND.IND sent",found : false},
+            {details : "CM_MNBC_SOUND.IND sent",display : "CM_MNBC_SOUND.IND sent",found : false},
+            {details : "CM_MNBC_SOUND.IND sent",display : "CM_MNBC_SOUND.IND sent",found : false},
+            {details : "CM_MNBC_SOUND.IND sent",display : "CM_MNBC_SOUND.IND sent",found : false},
+            {details : "CM_MNBC_SOUND.IND sent",display : "CM_MNBC_SOUND.IND sent",found : false},
+            {details : "CM_MNBC_SOUND.IND sent",display : "CM_MNBC_SOUND.IND sent",found : false},
+            {details : "CM_MNBC_SOUND.IND sent",display : "CM_MNBC_SOUND.IND sent",found : false},
+            {details : "CM_MNBC_SOUND.IND sent",display : "CM_MNBC_SOUND.IND sent",found : false},
+            {details : "CM_ATTEN_CHAR.IND received",display : "CM_ATTEN_CHAR.IND received",found : false},
+            {details : "CM_ATTEN_CHAR.RSP sent",display : "CM_ATTEN_CHAR.RSP sent",found : false},
+            {details : "CM_VALIDATE.REQ sent",display : "CM_VALIDATE.REQ sent",found : false},
+            {details : "CM_SLAC_MATCH.CNF received. -Charger Not Ready-",display : "CM_SLAC_MATCH.CNF received. -Charger Not Ready-",found : false},	
+            {details : "CM_SLAC_MATCH.CNF received. -Charger Ready-",display : "CM_SLAC_MATCH.CNF received. -Charger Ready-",found : false},	
+            {details : "CM_SLAC_MATCH.CNF received. -Charger Success-",display : "CM_SLAC_MATCH.CNF received. -Charger Success-",found : false},	
+            {details : "CM_SET_KEY.CNF received",display : "CM_SET_KEY.CNF received",found : false},	
+            {details : "New keys set",display : "New keys set",found : false},	
+            {details : "Link Measurement:",display : "Link Measurement:",found : false},	
+            {details : "Sending IPv6.",display : "Sending IPv6.",found : false},	
+            {details : "IPv6 Message is received",display : "IPv6 Message is received",found : false},	
+
+            
+
         ]
     }
 
@@ -115,7 +132,7 @@ class DeviceState extends Component{
 
             {
                 this.pevStrings.map((value,key)=>(
-                   this.prepareTimeLineItem(value.details,value.found,key)                      
+                   this.prepareTimeLineItem(value.display,value.found,key)                      
                 ))
             }
 
@@ -140,51 +157,57 @@ class DeviceState extends Component{
     }
 
     ProcessSplittedCommand(sCommand){
-
-        switch(sCommand)
-        {
-            case "ROLE:PEV":
-                this.role = "PEV";
-                clearInterval(this.queryInterval);
-                this.setState({  printOutCom : this.printOutCom,
-                                    toggleToRefresh : this.toggleToRefresh,
-                                    role : this.role  
-                                })
-
-                
-
-                break;
-            case "ROLE:EVSE":
-                this.role = "EVSE";
-                clearInterval(this.queryInterval);
-                this.setState({  printOutCom : this.printOutCom,
-                                    toggleToRefresh : this.toggleToRefresh,
-                                    role : this.role  })
-                break;
-            default:
-                let nowFound = false;
-                for(let i=0;i<this.pevStrings.length;i++){
-                    console.log("Compare ",sCommand, " with ",this.pevStrings[i].details)
-                    if(sCommand.includes(this.pevStrings[i].details) && this.pevStrings[i].found === false){
-                        this.pevStrings[i].found = true
-                        nowFound = true
-                        break;
+        console.warn("sCommand ",sCommand)
+        if(sCommand.length > 5){
+            if(sCommand.includes("ROLE:PEV"))
+            {
+                    this.role = "PEV";
+                    clearInterval(this.queryInterval);
+                    this.setState({  printOutCom : this.printOutCom,
+                                        toggleToRefresh : this.toggleToRefresh,
+                                        role : this.role  
+                                    })
+            }
+            else if(sCommand.includes("ROLE:EVSE"))
+            {
+                    this.role = "EVSE";
+                    clearInterval(this.queryInterval);
+                    this.setState({  printOutCom : this.printOutCom,
+                                        toggleToRefresh : this.toggleToRefresh,
+                                        role : this.role  })
+            }else{
+                    let nowFound = false;
+                    for(let i=0;i<this.pevStrings.length;i++){
+                        //console.log("Compare ",sCommand, " with ",this.pevStrings[i].details)
+                        if(sCommand.includes(this.pevStrings[i].details) && this.pevStrings[i].found === false){
+                            if(sCommand.includes(this.pevStrings[24].details))//Link Measurement
+                            {
+                                let measurementValue = sCommand.split(':')
+                                if(measurementValue.length>1){
+                                    this.pevStrings[24].display = "Link Measurement: "+Number(measurementValue[1].trim())+"ms."
+                                }
+                            }
+                           
+                            this.pevStrings[i].found = true
+                            nowFound = true
+                            break;
+                        }
                     }
-                }
 
-                if(nowFound === true){
-                    console.warn("OKKKKKKKK")
+                    if(nowFound === true){
+                        console.warn("OKKKKKKKK")
 
-                    this.setState({  
-                        printOutCom : this.printOutCom,
-                        toggleToRefresh : true,
-                        role : this.role  
-                    })
-
-                }
+                        this.setState({  
+                            printOutCom : this.printOutCom,
+                            toggleToRefresh : true,
+                            role : this.role  
+                        })
+                    }
+            }
+        }
+        
 
                        
-        }
     }
 
 
@@ -220,7 +243,7 @@ class DeviceState extends Component{
         console.log("Port..",this.props.port)
         const p_Port = this.props.port
 
-        await p_Port.port.open({ baudRate: 9600});
+        await p_Port.port.open({ baudRate: 115200});
         this.portReader = p_Port.port.readable.getReader();
 
         this.portWriter = p_Port.port.writable.getWriter();
@@ -230,12 +253,12 @@ class DeviceState extends Component{
             this.ReadValues()
         },0)
 
-        this.queryInterval = setInterval(()=>{
-            const data = new Uint8Array([114]); // r
-            this.portWriter.write(data).then(res=>{
-            });
-            
-        },100)
+        const data = new Uint8Array([114]); // r
+        this.portWriter.write(data).then(res=>{
+        });
+        console.log("ask role")
+        
+       
         console.log("OpenReadComPort | end")
     }
 
@@ -259,6 +282,7 @@ class DeviceState extends Component{
         this.initializePEVStrings()
         this.pevSendStartCommand = this.pevSendStartCommand.bind(this)
         this.pevSendStopCommand = this.pevSendStopCommand.bind(this)
+        this.linkMeasurements = []
     }
     render(){
         return (
